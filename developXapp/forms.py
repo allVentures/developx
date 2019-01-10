f8e53026c3bddb3b6887e1c6870de0ac8900b4c6
allVentures <paul@allphotobangkok.com>
2019-01-10 17:54:39 +0100

    notes function in Django view

--- developXapp/forms.py
+++ developXapp/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 
 class CashAmount(forms.Form):
-    cashamount = forms.IntegerField(label="Username", widget=forms.NumberInput(attrs={"class": "form-control"}))
+    cashamount = forms.FloatField(label="Username", required=False, widget=forms.NumberInput(attrs={"class": "form-control"}))
+
+# Im leaving the Float Field in this form on purpose, otherwise Django would validate NumberInput and would not allow to submit form
+# so I would not be able to throw exception in Django
+# also Im assuming that we are talking about some ATM keyboard so the user can not input other characters like letters etc.
+
--- developXapp/views.py
+++ developXapp/views.py
@@ -5,6 +5,30 @@ from developXapp.forms import CashAmount
 
 
 class MainPage(View):
+    @staticmethod
+    def notes_delivery(amount):
+        avilable_notes = [10, 20, 50, 100]
+        print("amount:",amount)
+        try:
+            if amount == None:
+                raise Exception([])
+            if amount <= 0:
+                raise Exception("InvalidArgumentException")
+            if amount % 10 != 0:
+                raise Exception("NoteUnavailableException")
+            sub_amount = amount
+            notes = []
+            x = len(avilable_notes)-1
+            while sub_amount > 0:
+                while x >= 0:
+                    while sub_amount >= avilable_notes[x]:
+                        sub_amount = sub_amount - avilable_notes[x]
+                        notes.append(avilable_notes[x])
+                    x = x - 1
+        except Exception as e:
+            notes = e
+        return notes
+
     def get(self, request):
         form = CashAmount()
         ctx = {"form": form}
@@ -15,11 +39,11 @@ class MainPage(View):
         if form.is_valid():
             cashamount = form.cleaned_data["cashamount"]
 
-            msg = cashamount
+            msg = MainPage.notes_delivery(cashamount)
             ctx = {"form": form, "msg": msg}
             return render(request, "main_page.html", ctx)
 
         else:
-            error = "Form invalid, please try again!"
-            ctx = {"form": form, "error": error}
+            msg = "Form invalid, please try again!"
+            ctx = {"form": form, "msg": msg}
             return render(request, "main_page.html", ctx)
